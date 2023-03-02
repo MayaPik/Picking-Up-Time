e2e3ef9150cc4e5ab759351246ee61e3ab70a244
mayapik <maya.pik@gmail.com>
2023-03-02 11:16:18 +0200

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,23 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { LoginScreen } from "./Pages/login/LoginScreen";
 import { MainScreen } from "./Pages/main/MainScreen";
 import { useStore } from "./store";
 import "./App.css";
 
-const App: React.FC = () => {
-  const navigate = useNavigate();
-  const server = useStore((state) => state.server);
-  const setUser = useStore((state) => state.setUser);
-  const setUsertype = useStore((state) => state.setUsertype);
+function App() {
   const isLoggedIn = useStore((state) => state.isLoggedIn);
-  const setIsLoggedIn = useStore((state) => state.setIsLoggedIn);
-
-  useEffect(() => {
-    setUsertype(localStorage.getItem("usertype"));
-  }, [setUsertype]);
-
-  useEffect(() => {
-    fetch(`${server}/api/user`, {
-      credentials: "include",
-    })
-      .then((response) => response.json())
-      .then((user) => {
-        console.log(user);
-        setUser(user);
-        setIsLoggedIn(true);
-        if (user.adminid) {
-          setUsertype("admin");
-        } else if (user.childid) {
-          setUsertype("child");
-        } else if (user.guideid) {
-          setUsertype("guide");
-        }
-      })
-      .catch((error) => console.log(error));
-  }, [setUser, setUsertype, setIsLoggedIn, server]);
 
   useEffect(() => {
     const checkUser = () => {
       if (isLoggedIn && window.location.pathname === "/") {
         window.location.href = "/main";
       } else if (!isLoggedIn) {
-        navigate("/");
+        window.location.href = "/";
       }
     };
     checkUser();
-  }, [isLoggedIn, navigate]);
+  }, [isLoggedIn]);
 
   return (
     <Router>
@@ -57,6 +27,6 @@ const App: React.FC = () => {
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
--- src/Pages/main/MainScreen.tsx
+++ src/Pages/main/MainScreen.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
 import { ParentScreen } from "./ParentScreen";
 import { AdminScreen } from "./AdminScreen";
 import { GuideScreen } from "./GuideScreen";
@@ -21,8 +22,35 @@ import "./mainscreen.css";
 export const MainScreen: React.FC = () => {
   const user = useStore((state) => state.user);
   const usertype = useStore((state) => state.usertype);
-  const setIsLoggedIn = useStore((state) => state.setIsLoggedIn);
+  // const navigate = useNavigate();
   const server = useStore((state) => state.server);
+  const setUser = useStore((state) => state.setUser);
+  const setUsertype = useStore((state) => state.setUsertype);
+  const setIsLoggedIn = useStore((state) => state.setIsLoggedIn);
+
+  useEffect(() => {
+    setUsertype(localStorage.getItem("usertype"));
+  }, [setUsertype]);
+
+  useEffect(() => {
+    fetch(`${server}/api/user`, {
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        console.log(user);
+        setUser(user);
+        setIsLoggedIn(true);
+        if (user.adminid) {
+          setUsertype("admin");
+        } else if (user.childid) {
+          setUsertype("child");
+        } else if (user.guideid) {
+          setUsertype("guide");
+        }
+      })
+      .catch((error) => console.log(error));
+  }, [setUser, setUsertype, setIsLoggedIn, server]);
 
   const PageDisplay: React.FC = () => {
     if (usertype === "child") {
